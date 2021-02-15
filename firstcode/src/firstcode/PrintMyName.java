462a6aa5db6e8c14181bf9c4da26bc11de7741b6
pc <pc@DESKTOP-T4D614M>
2021-02-15 23:39:12 +0300

    First commit

---
+++ firstcode/src/firstcode/PrintMyName.java
@@ -0,0 +1,10 @@
+package firstcode;
+
+public class PrintMyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My name is hussain ali");
+	}
+
+}
